f2113bc34b01549e9c38f9bbe70b426fde2f1135
Romy Schmidt <orca@Waldkauz.(none)>
2014-09-17 15:21:30 +0200

    Started to implement new stuff

--- fc.c
+++ fc.c
@@ -1,22 +1,36 @@
 #include <string.h>
 #include <sparrowNet.h>
 
-#define VERSION "1.0.0.0"
+#define VERSION "1.1.0.0"
+
+spNetC4AProfilePointer profile = NULL;
+
+int check_profile()
+{
+	profile = spNetC4AGetProfile();
+	if (profile == NULL)
+	{
+		printf("No profile file found!\n Please create on with a C4A Manager\n");
+		return 0;
+	}
+	return 1;
+}
 
 int main(int argc, char **argv)
 {
 	spInitNet();
 	printf("Fusilli client Version "VERSION"\n");
-	printf("Built for "SP_DEVICE_STRING"\n");	
-	spNetC4AProfilePointer profile = spNetC4AGetProfile();
-	if (profile == NULL)
-	{
-		printf("No profile file found!\n Please create on with a C4A Manager\n");
-		return -2;
-	}
+	printf("Built for "SP_DEVICE_STRING"\n");
+	if (check_profile == 0)
+		return -1;
 	if (argc < 3)
 	{
-		printf("Usage: fc GAMENAME SCORE [TIMEOUT (in ms, default is 10000)]\n");
+		//printf("Usage: fc GAMENAME SCORE [TIMEOUT (in ms, default is 10000)]\n");
+		printf("Usage: fc [OPTIONS] ACTION [ACTION OPTIONS]\n");
+		printf("These actions may be:\n");
+		printf("       fc [OPTIONS] push GAMENAME SCORE [TIMEOUT]\n");
+		printf("       * submits the score SCORE for the game GAMENAME using the optional\n");
+		printf("         timeout TIMEOUT (in ms). The default timeout is 10000 ms.\n");
 		return -1;
 	}
 	char* game_name = argv[1];
